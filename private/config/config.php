3254fd7680d4107dc2b99b9ff3caaf5f18d8a687
JFBizueque <71117126+JFBizueque@users.noreply.github.com>
2026-05-16 02:34:59 +0200

    Create config.php

---
+++ private/config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+define('BASE_URL', 'http://localhost');
+
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'crypto_pdf_shop');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+
+session_start();
